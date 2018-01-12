554ceb0b1923954868aa699e462c2d228afdb881
David Cardoso <davidcardoso.ti@gmail.com>
2018-01-11 21:59:38 -0300

    Refactored some stuffs... - updated readme - refactored bin/meou.sh - refactored install.sh - created bin/helpers.sh - created txt/mascot.txt - updated txt/help.txt

---
+++ bin/helpers.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+## @file helpers.sh
+## @brief      Helper Functions with block with commands.
+## @author     David Cardoso
+## @since      January 11th, 2018
+## @copyright  GNU GPL-3.0
+## @sa         https://github.com/KalahariDavid/my-env-on-ubuntu/
+
+# function case_option()
+case_option(){
+	case $2 in
+		--install)
+			install_$_block
+			;;
+		--help)
+			show_help
+			;;
+		--version)
+			show_version
+			;;
+		--mascot)
+			show_mascot
+			;;
+		"")
+			if [[ $1 == 'software' ]]; then
+				confirm_install
+			else
+				echo '...No matched option!'
+			fi
+			;;
+		*)
+			echo '...No matched option!'
+			;;
+	esac
+}
+
+# function confirm_install()
+confirm_install(){
+	echo "Do you want to install $_block? (Y/n)"
+	read choice
+	if [[ $choice == "Y" || $choice == "y" || $choice == "yes" ]];  then
+		install_$_block
+	else
+		echo "...Not installed!"	
+	fi
+}
+
+# function confirm_continue()
+confirm_continue(){
+	echo "Continue? (Y/n)"
+   	read choice;
+   	if [[ $choice != "Y" || $choice != "y" || $choice != "yes" ]];  then
+		exit 0
+	fi
+}
+
+# function show_help()
+show_help(){
+	show_line
+	cat $MEOUPATH/txt/help.txt
+	echo
+}
+
+# function show_version()
+show_version(){
+	show_line
+	cd $MEOUPATH && git fetch -vp 2&> /dev/null
+	echo "Version: "
+	git tag | egrep v.
+	cd - 2&> /dev/null
+	echo
+	show_mascot
+}
+
+# function show_mascot()
+show_mascot(){
+	cat $MEOUPATH/txt/mascot.txt
+	echo 
+}
+
+# function show_sharp_line()
+show_sharp_line(){
+	echo "#################################"
+}
+
+# function show_line()
+show_line(){
+	echo "---------------------------------"
+}
+
+# function show_header()
+show_header(){
+	clear
+	show_sharp_line
+	echo "#  Welcome to My Env on Ubuntu! #"
+	echo "#  Powered by David Cardoso.    #"
+	show_sharp_line
+	echo
+	echo "See also: https://github.com/KalahariDavid/my-env-on-ubuntu"
+	echo
+	echo "  Running $1 script..."
+	echo
+}
--- install.sh
+++ install.sh
@@ -11,14 +11,26 @@
 _script=$0 # name of this script
 _arg1=$1 # first argument
 
+# importing functions
+. ./bin/helpers.sh
+
+# header
+show_header "install.sh"
+
 # setting up the symbolic link to bin path
+show_line
+echo "Creating symbolic link 'meou' on /usr/local/bin/ path..."
 sudo rm /usr/local/bin/meou
 sudo ln -s $(pwd)/bin/meou.sh /usr/local/bin/meou
 
 # adding execution permission to the scripts in bin path
+show_line
+echo "Adding execution permission to the scripts in $PWD/bin path..."
 chmod +x ./bin/*.sh
 
 # setting up the user profile configs (to be persistent between users logins sessions)
+show_line
+echo "Setting up the user profile configs (to be persistent between users logins sessions)..."
 _bash="#!/bin/bash"
 echo "$_bash
 if [[ \$USER == $USER ]]; then
@@ -26,3 +38,13 @@ if [[ \$USER == $USER ]]; then
 fi" > ./txt/configs.txt
 sudo cp ./txt/configs.txt /etc/profile.d/my-env-on-ubuntu-configs-$USER.sh
 export MEOUPATH=$PWD
+rm -f ./txt/configs.txt
+
+# End of installation
+show_line
+echo "Congratulations! Now you have 'My Env on Ubuntu' installed!"
+echo
+show_mascot
+echo
+echo "Run 'meou --help' for more information!"
+echo
